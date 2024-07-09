1c68e4f9c0144f0e01c445472331dbf3af4eecb1
ziqi-jin <ziqijincoder@gmail.com>
2024-07-09 22:45:35 +0800

    modify iter 5 comment

--- iter_5_load_cot.py
+++ iter_5_load_cot.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     dspy.settings.configure(lm=lm) 
 
     print(f"## 创建并设置大模型 {model_name} ##")
-    print(f"\n## 对模板和参数进行调优 - start ##\n")
+    print(f"\n## 导入已优化模型参数示例 - start ##\n")
 
     class CoT(dspy.Module):
         def __init__(self):
@@ -29,4 +29,4 @@ if __name__ == '__main__':
     print(f"问题：{question} \n答案：{response.answer}")
     print("prompt: ")
     lm.inspect_history(n=1) if PROMPT_SWITCH else print("if you want see prompt, set PROMPT_SWITCH = True")
-    print(f"\n## 对模板和参数进行调优 - end ##\n")
+    print(f"\n## 导入已优化模型参数示例 - end ##\n")
